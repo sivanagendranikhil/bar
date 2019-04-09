7d7851f940f7fa481ecc123913cb6bc7ac079007
sivanagendranikhil <nikil542college@gmail.com>
2019-04-09 15:11:37 +0530

    1 file modified, 1 added

--- src/app/default/default.component.ts
+++ src/app/default/default.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StockService } from '../stock.service';
+import { MyData } from './my-data';
 
 @Component({
   selector: 'app-default',
@@ -11,7 +12,14 @@ export class DefaultComponent implements OnInit {
   callPrediction: string;
   putPrediction: string;
   finalPrediction: string;
+  strikePrice: Number;
   result: Number[];
+  stocks: MyData[];
+  message: string;
+  atmTodayPredict: string;
+  oiTodayPredict: string;
+  pdTodayPredict: string;
+  pcrTodayPredict: string;
 
   constructor(private stockService: StockService) { }
 
@@ -20,6 +28,8 @@ export class DefaultComponent implements OnInit {
       .then(
         (resp) => { 
           this.result = resp
+          // STRIKE PRICE
+          this.strikePrice = this.result[3]
           // FINAL PREDICTION
           if(this.result[0] == 1) {
             this.finalPrediction = "Bullish"
@@ -49,6 +59,49 @@ export class DefaultComponent implements OnInit {
           console.log('Error!!!') 
         }
       );
+
+      this.stockService.getData()
+    .then(
+      (stocks) => { 
+        this.stocks = stocks 
+        // ATM
+        if(this.stocks[this.stocks.length-1].atm[2] == 1) {
+          this.atmTodayPredict = "Bullish";
+        } else if(this.stocks[this.stocks.length-1].atm[2] == -1) {
+          this.atmTodayPredict = "Bearish";
+        } else {
+          this.atmTodayPredict = "Moving Sideways";
+        }
+        // OI 
+         if(this.stocks[this.stocks.length-1].oi[2]>this.stocks[this.stocks.length-1].oi[3]&&this.stocks[this.stocks.length-1].oi[2] >= 2) {
+          this.oiTodayPredict = "Bullish";
+        } else if(this.stocks[this.stocks.length-1].oi[2] < this.stocks[this.stocks.length-1].oi[3] && this.stocks[this.stocks.length-1].oi[3] >= 2) {
+          this.oiTodayPredict = "Bearish";
+        } else {
+          this.oiTodayPredict = "Moving Sideways";
+        } 
+        // Premium Decay
+        if(this.stocks[this.stocks.length-1].premiumDecay[2] == 1) {
+          this.pdTodayPredict = "Bullish";
+        } else if(this.stocks[this.stocks.length-1].premiumDecay[2] == -1) {
+          this.pdTodayPredict = "Bearish";
+        } else {
+          this.pdTodayPredict = "Moving Sideways";
+        } 
+        // Put Call Ratio
+        if(this.stocks[this.stocks.length-1].pcr[1] == 1) {
+            this.pcrTodayPredict = "Bullish";
+          } else if(this.stocks[this.stocks.length-1].pcr[1] == -1) {
+            this.pcrTodayPredict = "Bearish";
+          } else {
+            this.pcrTodayPredict = "Moving Sideways";
+          } 
+      },
+      (resp) => { 
+        this.message = resp.message;
+        console.log('Error!!!') 
+      }
+    );
   }
 
 }
